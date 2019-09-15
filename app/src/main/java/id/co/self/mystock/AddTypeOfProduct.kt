346462b73c33426601e9b9a793a65b65a746ca63
kusumajati51@gmail.com <kusuma12>
2019-09-16 01:10:12 +0700

    init

---
+++ app/src/main/java/id/co/self/mystock/AddTypeOfProduct.kt
@@ -0,0 +1,14 @@
+package id.co.self.mystock
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddTypeOfProduct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
--- app/src/main/java/id/co/self/mystock/RegistrationActivity.kt
+++ app/src/main/java/id/co/self/mystock/RegistrationActivity.kt
@@ -1,12 +1,95 @@
 package id.co.self.mystock
 
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ActionMode
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.DialogTitle
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.result.Result
+import id.co.self.mystock.session.PreferenceHelper
+import kotlinx.android.synthetic.main.activity_registration.*
+import org.json.JSONObject
+import java.lang.Exception
+import java.nio.charset.Charset
 
 class RegistrationActivity : AppCompatActivity() {
-    internal var RegisterURL = "http://192.168.43.223:3000/api/v1/register"
+    internal var registerURL = "http://192.168.43.223:3000/api/v1/register"
+    lateinit var pref: PreferenceHelper
+    val regTask = 1
+    lateinit var mProgressDialog: ProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        mProgressDialog = ProgressDialog(this@RegistrationActivity)
+        mProgressDialog.setMessage("Halooo")
+        pref = PreferenceHelper(this)
+        if(pref!!.getIsLogin()){
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK )
+            startActivity(intent)
+        }
+
+        btn.setOnClickListener {
+
+            register()
+
+        }
     }
+
+    @Throws(Exception::class)
+    private fun register(){
+        mProgressDialog.show()
+        try {
+            Fuel.post(registerURL, listOf("name" to name!!.text.toString(),
+                "email" to email!!.text.toString(),
+                "no_hp" to phone_number!!.text.toString(),
+                "password" to password!!.text.toString(),
+                "password_confirmation" to password_conrimation!!.text.toString())
+            ).responseJson { request, response, result ->
+//                val getResponse:String = result.get().content;
+//                Log.d("plzzzzz", getResponse)
+//                val jObject = JSONObject(getResponse)
+//                mProgressDialog.dismiss()
+                when(result){
+                    is Result.Failure ->{
+                        val ex = result.getException()
+                        val e = ex.errorData.toString(Charsets.UTF_8)
+                        Toast.makeText(applicationContext, e ,Toast.LENGTH_LONG).show()
+                        println(ex)
+                    }
+                    is Result.Success ->{
+                        val data = result.get()
+                        Toast.makeText(applicationContext, data.content,Toast.LENGTH_LONG).show()
+                        println(data)
+                    }
+                }
+                mProgressDialog.dismiss()
+
+            }
+        }catch (e: Exception){
+            e.printStackTrace()
+            Toast.makeText(applicationContext,e.message,Toast.LENGTH_SHORT).show()
+        }
+        catch (f: FuelError) {
+            f.printStackTrace()
+            Toast.makeText(applicationContext,f.message,Toast.LENGTH_SHORT).show()
+        }
+        finally {
+
+        }
+    }
+
+    fun saveInfo(jObject: JSONObject){
+
+    }
+
+
 }
